553c190f5190497231cec070f7999d645b5ed980
Slava Stopul <st.sviat@gmail.com>
2026-07-06 13:09:29 +0100

    Remove dead code

--- lbm/src/core/analytical.py
+++ lbm/src/core/analytical.py
@@ -14,7 +14,6 @@ def poiseuille_profile(Ny, g_x, nu):
     numpy.ndarray: The analytical Poiseuille flow profile as a 1D array of length Ny.
     """
     y = np.arange(1, Ny-1)
-    u_max = g_x * (Ny - 1)**2 / (8 * nu)
     u_profile = (g_x/(2*nu)) * (y - 0.5) * (Ny - 1.5 - y)
     
     return u_profile
