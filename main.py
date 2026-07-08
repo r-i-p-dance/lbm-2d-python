9c2ad8b12db633b429b7e582d0d6374486857b1b
Slava Stopul <st.sviat@gmail.com>
2026-07-08 12:59:35 +0100

    Update convergence plot after every profile comparison, run resolutions up to 1024

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from lbm.src.study.convergence import run_convergence_study
 from lbm.src.plot.profile import plot_profile_comparison
 from lbm.src.plot.convergence import plot_convergence
 
-RESOLUTIONS = [16, 32, 64, 128]
+RESOLUTIONS = [16, 32, 64, 128, 256, 512, 1024]
 
 # Run the study
 results = run_convergence_study(RESOLUTIONS, tau=0.8, u_max=0.04)
 
 L2_errors = []
 # Produce the reports
-for r in results:
+for i, r in enumerate(results):
     plot_profile_comparison(r.u_numerical, r.u_analytical, r.Ny,
                             save_path=f"results/profile_Ny{r.Ny}.png")
     L2_errors.append(r.L2_error)
 
-plot_convergence(RESOLUTIONS, L2_errors, save_path="results/convergence.png")
+    plot_convergence(RESOLUTIONS[:i+1], L2_errors, save_path="results/convergence.png")
+
