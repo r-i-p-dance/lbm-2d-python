ebeee67a5daa87466fc3b8d34f2f96abb0511372
Slava Stopul <st.sviat@gmail.com>
2026-08-14 20:32:33 +0100

    Add colormap and interpolation inputs for Recorder class

--- lbm/src/plot/animation.py
+++ lbm/src/plot/animation.py
@@ -5,17 +5,16 @@ import numpy as np
 
 
 class Recorder:
-    def __init__(self, lattice, path, fps=15, dpi=100, every=100):
+    def __init__(self, lattice, path, fps=15, dpi=100, every=100, cmap='RdBu_r', interpolation='none'):
         self.lattice = lattice
         self.every = every
-        
         aspect = lattice.nx / lattice.ny
         self.fig, self.ax = plt.subplots(figsize=(6*aspect, 6))
         self.img = self.ax.imshow(
             np.zeros((lattice.nx, lattice.ny)).T,
-            cmap='RdBu_r',
+            cmap=cmap,
             vmin=0.0, vmax=lattice.u_max,
-            interpolation='bilinear',
+            interpolation=interpolation,
         )
         self.ax.set_axis_off()
         self.fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
@@ -28,13 +27,15 @@ class Recorder:
             vel = np.sqrt(self.lattice.ux**2 + self.lattice.uy**2)
             self.img.set_data(vel.T)
             self.writer.grab_frame()
+            return True
+        return False
 
     def close(self):
         self.writer.finish()
 
-def record_development(case_class, nx, ny, tol, every=100, path=None, **case_kwargs):
+def record_development(case_class, nx, ny, tol, every=100, path=None, cmap='RdBu_r', interpolation='none', **case_kwargs):
     ltc = case_class(nx=nx, ny=ny, **case_kwargs)
-    rec = Recorder(ltc, path=path, every=every)
+    rec = Recorder(ltc, path=path, every=every, cmap=cmap, interpolation=interpolation)
     ltc.converge(tol=tol, recorder=rec)
     rec.close()
 
