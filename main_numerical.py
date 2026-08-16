5758696d4fa6081572027bf4d7fbdd5ac79917db
Slava Stopul <st.sviat@gmail.com>
2026-08-16 13:35:05 +0100

    Update to run all these study cases and record simulations

--- main_numerical.py
+++ main_numerical.py
@@ -1,18 +1,24 @@
 from lbm.src.plot.convergence import plot_convergence
-from lbm.src.plot.animation import record_development
 from lbm.src.study.grid_convergence import run_grid_convergence_study
-from lbm.src.cases.two_rectangles import TwoRectangles
+
 from lbm.src.plot.animation import Recorder
+from lbm.src.plot.animation import record_development
+
+# Cases
+from lbm.src.cases.two_rectangles import TwoRectangles
+from lbm.src.cases.backward_step import BackwardStep
+from lbm.src.cases.cylinder import Cylinder
 
 
-RESOLUTIONS = [16, 32, 64, 128]
+RESOLUTIONS = [32, 64, 128, 256]
 Re          = 10.0
+lbm_tau     = 0.6
 tol         = 1e-8
 
 # Run the study
-for case in [TwoRectangles]:
-    print(f"Running grid convergence study for {case.__name__}…")
-    results = run_grid_convergence_study(case, RESOLUTIONS, Re=Re, tol=tol)
+for case in [TwoRectangles, BackwardStep, Cylinder]:
+    print(f"\nRunning grid convergence study for {case.__name__} at Re={Re}…")
+    results = run_grid_convergence_study(case, RESOLUTIONS, Re=Re, lbm_tau=lbm_tau, tol=tol)
 
     L2_errors = []
 
@@ -20,22 +26,17 @@ for case in [TwoRectangles]:
     for i, r in enumerate(results):
         L2_errors.append(r.L2_error)
 
-        plot_convergence(RESOLUTIONS[:i+1], L2_errors, save_path=f"results/plots/two_rectangles/convergence_{case.__name__}.png")
+        plot_convergence(RESOLUTIONS[:i+1], L2_errors, save_path=f"results/plots/{case.__name__}/convergence_{case.__name__}_Re{int(Re)}.png")
 
-steps = 3000
+Re = 20.0
+RESOLUTIONS = [128]
+vmax_factors = [4.0, 0.8, 0.4]
 
-for case in [TwoRectangles]:
+for i, case in enumerate([TwoRectangles, BackwardStep, Cylinder]):
     for r in RESOLUTIONS:
-        ltc = case(ny=r, Re=Re)
-        rec = Recorder(ltc, path=f"results/animations/two_rectangles/anim_Ny{r}_{case.__name__}.gif", every=5, interpolation='none')
 
-        for step in range(steps):
-            ltc.step()
-            if rec.maybe_capture(step):
-                print(f"\r{step}/{steps}", end="")
-        rec.close()
-
-        # record_development(case, nx=5*r, ny=r, Re = Re, tol=tol, every=1,
-        #                    path=f"results/animations/two_rectangles/anim_Ny{r}_{case.__name__}.gif",
-        #                    interpolation='none')
+        record_development(case, nx=5*r, ny=r, Re=Re, tau_lbm=0.6, max_steps=100_000, every=100, 
+                           path=f"results/animations/{case.__name__}/anim_Ny{r}_{case.__name__}_Re{int(Re)}.gif",
+                           vmax_factor=vmax_factors[i], cmap='RdBu_r', interpolation='bilinear', 
+                           print_progress=False)
 
